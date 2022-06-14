d9c36997ad7dcaacbb06f96eba48e922536b18b3
wrefiyu <98194489+yolanda2010@users.noreply.github.com>
2022-06-14 20:25:04 +0800

    Update about.h

--- src/about.h
+++ src/about.h
@@ -1,12 +1,14 @@
 /*
- * Copyright (C) 2022 Pisces system Team.
+ * Copyright (C) 2022 Pisces system  Team.
  *
- * Author:     Lanka Yan <young3030@163.com>
+ * Author:     Lunisia Yan  <young3030@163.com>   Sumuli <sumuli>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
+ *
+ * fork from Cutefish.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
